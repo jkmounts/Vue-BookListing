5be8630ee2a4486c9e37c2b95e5e2c42dba3c7f1
Jestin Mounts <jkmounts@gmail.com>
2021-08-24 08:19:18 -0400

    Module 3

---
+++ src/components/BookItem.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <li>
+            {{ book.title }}:{{ book.author }}
+        </li>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BookItem",
+        props: ['book']
+    }
+</script>
+
+<style scoped>
+
+li {
+    display: block;
+    margin: 0 10px;
+}
+
+</style>
